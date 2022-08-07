b7988d49a5cbac4ab2a577db1a1110a2aebe8eed
David <david.programming@protonmail.com>
2022-08-07 14:47:21 +0200

    feat: animation loop with a player

--- src/index.js
+++ src/index.js
@@ -7,3 +7,39 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 const c = canvas.getContext('2d');
 
+// ===================== CLASSES =====================
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 500,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillStyle = 'black';
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+// ===================== VARIABLES =====================
+const player = new Player();
+
+// ===================== ANIMATION LOOP =====================
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+// ===================== EXECUTION =====================
+animate();
