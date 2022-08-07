75ae8f6b8404211ca63b9ee250cbf89b8efee4bc
David <david.programming@protonmail.com>
2022-08-07 14:39:09 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles/main.scss';
+import './scripts/utils.js';
+
+// ===================== CANVAS =====================
+const canvas = document.querySelector('#canvas');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+const c = canvas.getContext('2d');
+
